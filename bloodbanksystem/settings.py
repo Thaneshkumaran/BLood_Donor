8f3bfef6b93f2fb519efd677dd8594bb1ba4b5f3
Thaneshkumaran R <thaneshr42@gmail.com>
2025-04-24 11:06:03 +0530

    static file

--- bloodbanksystem/settings.py
+++ bloodbanksystem/settings.py
@@ -72,7 +72,7 @@ DATABASES = {
         'NAME': 'bbdmspythondb',
         'USER': 'root',
         'PASSWORD': '123456',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
         
     }
