8fc44a79d6744ffd3bd4869bbb52fa80c48b2bea
Thaneshkumaran R <thaneshr42@gmail.com>
2025-04-24 11:00:21 +0530

    static file

--- bloodbanksystem/settings.py
+++ bloodbanksystem/settings.py
@@ -115,9 +115,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # make sure this directory is writable
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
